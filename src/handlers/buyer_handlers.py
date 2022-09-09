f9a241eb364789d42c43f2700a079fbef5f645e2
Daria <dashazoueva@gmail.com>
2022-09-09 11:04:32 +0300

    klava

--- src/handlers/buyer_handlers.py
+++ src/handlers/buyer_handlers.py
@@ -1,8 +1,9 @@
 from aiogram import types
+from aiogram.types import ReplyKeyboardRemove
 from keyboards import commands_default_keyboard
 from loader import dp
 
-@dp.message_handler(text=['Hello', 'Начать'])
+@dp.message_handler(text=['Hello', 'Начать', 'hello','начать', 'привет'])
 @dp.message_handler(commands="start")
 async def answer_start_command(message: types.Message):
 	# print(message ['from'] ['first_name'])
@@ -13,9 +14,13 @@ async def answer_start_command(message: types.Message):
 async def answer_help_command(message: types.Message):
 		await message.answer(text = f"Список команд представлен на клавиатуре", reply_markup=commands_default_keyboard)
 
-@dp.message_handler(text=["Огурцы"])
-async def answer_start_command(message: types.Message):
-		await message.answer(text = f"Огурцы Луховицкие")
+@dp.message_handler(commands=["items"])
+async def answer_items_command(message: types.Message):
+		await message.answer(text = f'У нас в наличии:'
+                                    '\n-Огурцы Луховицкие'
+                                    '\n-Помидоры черри'
+                                    '\n-Редис сезонный',
+                                    reply_markup=ReplyKeyboardRemove()) #убираем клавиатуру с экрана, чтобы только на команде help она была
 
 @dp.message_handler(text=["Помидоры"])
 async def answer_start_command(message: types.Message):
--- src/keyboards/defaults/buyer_keyboards.py
+++ src/keyboards/defaults/buyer_keyboards.py
@@ -3,9 +3,15 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 commands_default_keyboard = ReplyKeyboardMarkup(
  keyboard=[
         [
-            KeyboardButton(text = 'start')
+            KeyboardButton(text = '/start'),
+            KeyboardButton(text = '/items') #здесь кнопки клавиатуры в одну строку
+        ],
+        [
+            KeyboardButton(text = '/help') #здесь создается второй подсписок - вторая строка кнопок
+           
         ]
-    ]
+    ],
+    resize_keyboard=True
  
 )
 
