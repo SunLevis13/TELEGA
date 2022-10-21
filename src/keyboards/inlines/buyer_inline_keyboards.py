d21388a0732e038ce362e2c2fdd8abb5b4f76dc5
Daria <dashazoueva@gmail.com>
2022-10-21 18:52:58 +0300

    inline

--- src/keyboards/inlines/buyer_inline_keyboards.py
+++ src/keyboards/inlines/buyer_inline_keyboards.py
@@ -13,27 +13,29 @@ def get_item_inline_keyboard(item_index='0', status='Small') -> InlineKeyboardMa
                                                           for_data='items', # указываем, что именно для этой кнопки присвоено значение items под индексом ниже
                                                          id=index_left) # указываем индекс того товара, по которому надо вывести инфу
             )
-            item_inline_keyboard.add()
+            item_inline_keyboard.add(btn)
             
         case 'Small': # статус Small означает, что не выводим кнопку "влево"
             index_right = str(int(item_index)+1)
-            item_inline_keyboard.add(InlineKeyboardButton(text='>>>',
+            btn = InlineKeyboardButton(text='>>>',
                                                           callback_data=navigation_callback.new(
                                                           for_data='items', # указываем, что именно для этой кнопки присвоено значение items под индексом ниже
                                                           id=index_right)
-            ))
+            )
+            item_inline_keyboard.add(btn)
         case _: # любой другой статус сигнализирует, что можно выводить обе две кнопки "влево" и "вправо"
             index_left = str(int(item_index)-1)
             index_right = str(int(item_index)+1)
-            item_inline_keyboard.add(InlineKeyboardButton(text='<<<',
+            btn_left = InlineKeyboardButton(text='<<<',
                                                           callback_data=navigation_callback.new(
                                                           for_data='items',
-                                                         id=index_left)))
-            item_inline_keyboard.add(InlineKeyboardButton(text='>>>',
+                                                         id=index_left))
+            btn_right = InlineKeyboardButton(text='>>>',
                                                           callback_data=navigation_callback.new(
                                                           for_data='items',
-                                                          id=index_right)))
-    return get_item_inline_keyboard
+                                                          id=index_right))
+            item_inline_keyboard.row(btn_left,btn_right)
+    return get_item_inline_keyboard # функция вернет сформированную клавиатуру
 
 start_inline_keyboard = InlineKeyboardMarkup(row_width=1, # ширина строки - измеряется в количестве кнопок
                                             inline_keyboard=[
--- src/keyboards/inlines/callback_data.py
+++ src/keyboards/inlines/callback_data.py
@@ -2,4 +2,4 @@ from aiogram.utils.callback_data import CallbackData
 
 start_callback = CallbackData('start') # создали экземпляр класса CallbackData, через него зашиваем данные - создаем заголовок start
 
-navigation_callback = CallbackData('navigation_btn','for_data','id') # создаем поле for_data, чтолбы понимать для какой табл. из нашей базы данных мы создаем навигацию
+navigation_callback = CallbackData('navigation_btn','for_data','id') # создаем класс с полями. Поле for_data, чтолбы понимать для какой табл. из нашей базы данных мы создаем навигацию
